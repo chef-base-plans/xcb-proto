e631d96fef6b44dc2b383ebe38874384fd25730c
rybons <bonnerrk@gmail.com>
2021-03-18 17:50:15 -0400

    Bumped version 1.12 -> 1.13

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_name=xcb-proto
 pkg_origin=core
-pkg_version=1.12
+pkg_version=1.13
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="X11 client library"
 pkg_upstream_url="https://www.x.org/"
 pkg_license=('MIT')
 pkg_source="https://www.x.org/releases/individual/xcb/${pkg_name}-${pkg_version}.tar.bz2"
-pkg_shasum="5922aba4c664ab7899a29d92ea91a87aa4c1fc7eb5ee550325c3216c480a4906"
+pkg_shasum="7b98721e669be80284e9bbfeab02d2d0d54cd11172b72271e47a2fe875e2bde1"
 pkg_build_deps=(
   core/gcc
   core/libxml2
